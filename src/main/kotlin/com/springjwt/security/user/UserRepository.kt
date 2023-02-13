06a0407501699f365b10c3412e7c7ac9b49e98b7
David Vasconcelos <dsv10@hotmail.com>
2023-02-13 17:16:01 -0300

    repository

---
+++ src/main/kotlin/com/springjwt/security/user/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.springjwt.security.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User, Int> {
+
+    fun findByEmail(email: String): User?
+}
