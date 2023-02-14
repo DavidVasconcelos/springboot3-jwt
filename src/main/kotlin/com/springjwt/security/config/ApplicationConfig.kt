607392dad4fa5586754244c6fda13c65aedb9745
David Vasconcelos <dsv10@hotmail.com>
2023-02-13 22:12:21 -0300

    application config

--- src/main/kotlin/com/springjwt/security/config/ApplicationConfig.kt
+++ src/main/kotlin/com/springjwt/security/config/ApplicationConfig.kt
@@ -3,8 +3,14 @@ package com.springjwt.security.config
 import com.springjwt.security.user.UserRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
 
 @Configuration
 class ApplicationConfig(val repository: UserRepository) {
@@ -14,4 +20,15 @@ class ApplicationConfig(val repository: UserRepository) {
         repository.findByEmail(username) ?: throw UsernameNotFoundException("User not found")
     }
 
+    @Bean
+    fun authenticationProvider(): AuthenticationProvider {
+        val provider = DaoAuthenticationProvider()
+        provider.setUserDetailsService(this.userDetailsService())
+        provider.setPasswordEncoder(BCryptPasswordEncoder())
+        return provider
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager = config.authenticationManager
 }
---
+++ src/main/kotlin/com/springjwt/security/config/SecurityConfiguration.kt
@@ -0,0 +1,33 @@
+package com.springjwt.security.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration(val jwtAuthenticationFilter: JWTAuthenticationFilter,
+    val authenticationProvider: AuthenticationProvider) {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain = http
+        .csrf()
+        .disable()
+        .authorizeHttpRequests()
+        .requestMatchers("/api/v1/auth/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+        .build()
+}
